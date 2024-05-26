72be75bac9965769c0669802137373d3e75909c5
Jansonci <2849243773@qq.com>
2024-05-26 09:45:55 +0800

    Merge remote-tracking branch 'origin/master'

--- Kotlin-Android/JansiSnack_Front/app/src/main/java/com/example/jetsnack/model/Ingredient.kt
+++
@@ -1,4 +0,0 @@
-package com.example.jetsnack.model
-
-class Ingredient {
-}
--- Kotlin-Android/JansiSnack_Front/app/src/main/java/com/example/jetsnack/model/User.kt
+++ Kotlin-Android/JansiSnack_Front/app/src/main/java/com/example/jetsnack/model/User.kt
@@ -5,24 +5,17 @@ data class User(
     val name: String,
     val motto: String,
     val liked: Int,
-//    val age: Int,
     val follower: Int,
     val following: Int,
-//    val job: String,
-//    val location: String,
     val preferences: List<Filter>,
     val info: List<String>,
     val collections: List<Snack>,
-//    val posts: List<String>,
-//    val likes: List<String>
-    ) {
-}
+    )
 
 val Kygo = User(
     "Kygo",
     "Love what you love.",
     3,
-//    28,
     139,
     20,
     listOf(
@@ -35,8 +28,5 @@ val Kygo = User(
 
     ),
     listOf("男","21岁","挪威","电子音乐制作人"),
-
     listOf(snacks[0],snacks[1], snacks[2]),
-//    listOf(""),
-//    listOf("")
 )
