24c4cc7ee902798570740a80af54b5a8f3edb0e0
Jansonci <2849243773@qq.com>
2024-05-30 15:29:00 +0800

    Merge remote-tracking branch 'origin/master'

--- Kotlin-Android/JansiSnack_Front/app/src/main/java/com/example/jetsnack/api/WebSocketViewModel.kt
+++ Kotlin-Android/JansiSnack_Front/app/src/main/java/com/example/jetsnack/api/WebSocketViewModel.kt
@@ -1,9 +1,12 @@
 package com.example.jetsnack.api
+import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.receiveAsFlow
 import kotlinx.coroutines.launch
+import okhttp3.Response
 import okhttp3.WebSocket
 import okhttp3.WebSocketListener
 
@@ -13,22 +16,55 @@ class WebSocketViewModel : ViewModel() {
     private val webSocketClient = WebSocketClient()
 
     init {
-        this.webSocketClient.setListener(object : WebSocketListener() {
-            override fun onMessage(webSocket: WebSocket, text: String) {
-                viewModelScope.launch {
-                    messageChannel.send(text)  // 将接收的消息发送到 Channel
-                }
+        connectWebSocket()
+        Log.i("yyy", "WebSocketViewModel Created")
+    }
+
+    private fun connectWebSocket() {
+        viewModelScope.launch {
+            try {
+                this@WebSocketViewModel.webSocketClient.setListener(object : WebSocketListener() {
+                    override fun onMessage(webSocket: WebSocket, text: String) {
+                        viewModelScope.launch {
+                            messageChannel.send(text)  // 将接收的消息发送到 Channel
+                        }
+                    }
+
+                    override fun onFailure(
+                        webSocket: WebSocket,
+                        t: Throwable,
+                        response: Response?
+                    ) {
+                        // 处理连接失败
+                        viewModelScope.launch {
+                            messageChannel.send("Connection failed: ${t.message}")
+                            retryConnection()
+                        }
+                        // 重试连接
+                    }
+                })
+                this@WebSocketViewModel.webSocketClient.start("ws://10.0.2.2:8080/article/websocket")
+            } catch (e: Exception) {
+                Log.e("WebSocketError", "Failed to connect: ${e.message}")
+//                messageChannel.send("Failed to connect: ${e.message}")
+                retryConnection()
             }
-        })
-        this.webSocketClient.start("ws://10.0.2.2:8080/article/websocket")
+        }
+    }
+
+    private suspend fun retryConnection() {
+        delay(5000)  // 延迟5秒后重试连接
+        connectWebSocket()
     }
 
     fun sendMessage(message: String) {
         this.webSocketClient.send(message)
+        Log.i("yyysending", message)
     }
 
     override fun onCleared() {
         super.onCleared()
         this.webSocketClient.close()
+        Log.i("yyy", "WebSocketViewModel Destroyed")
     }
 }
